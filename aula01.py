07e8aa975cf2bf46f97c668ac1d495796f93659c
Jacqueline Navarro <154712329+jacquelinenavarro@users.noreply.github.com>
2024-06-05 18:37:33 +0000

    05062024

---
+++ aula01.py
@@ -0,0 +1,6 @@
+print ('Olá Mundo!')
+
+num1 = float(input('Digite o primeiro número: '))
+num2 = float(input('Digite o segundo número: '))
+soma = num1 + num2
+print ('A soma dos dois números é {}'.format(soma))
