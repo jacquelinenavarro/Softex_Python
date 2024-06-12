238ec794d3b4ee4541dd3d5a0bd40fbd0293aec3
Jacqueline Navarro <154712329+jacquelinenavarro@users.noreply.github.com>
2024-06-12 18:39:06 +0000

    aula4.120624

---
+++ aula042.py
@@ -0,0 +1,8 @@
+#Padaria
+
+manteiga = input('Digite o nome da manteiga: ')
+leite = input('Digite a marca do leite: ')
+queijoCoalho = input('Quantos quilos de queijo?: ')
+biscoito = input('Digite a marca do biscoito:')
+
+print (f'Manteiga: {manteiga}\nLeite: {leite}\nQueijo Coalho: {queijoCoalho}\nBiscoito: {biscoito}')
