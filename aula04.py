17c4ed3e9bdce98805b97142e8a02bb7522ac888
Jacqueline Navarro <154712329+jacquelinenavarro@users.noreply.github.com>
2024-06-12 18:36:49 +0000

    aula12062024

---
+++ aula04.py
@@ -0,0 +1,13 @@
+lista = [1, 2, 3]
+print(type(lista))
+print(lista)
+
+dicionario = {"a": 1, "b": 2}
+print(type(dicionario))
+print(dicionario)
+
+y = "Olá, mundo!"
+print(type(y))
+
+z = True
+print(type(z))
