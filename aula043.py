791d15e2813e88d2cfa94a4165c026797600efad
Jacqueline Navarro <154712329+jacquelinenavarro@users.noreply.github.com>
2024-06-14 01:21:57 +0000

    aula0434

--- aula043.py
+++ aula043.py
@@ -24,16 +24,37 @@
 
 # 3. Desenvolva um algoritmo que leia três números e exiba o maior deles.
 
-num1 = int(input("Digite o primeiro número: "))
-num2 = int(input("Digite o segundo número: "))
-num3 = int(input("Digite o terceiro número: "))
+# num1 = int(input("Digite o primeiro número: "))
+# num2 = int(input("Digite o segundo número: "))
+# num3 = int(input("Digite o terceiro número: "))
 
-if num1 > num2 and num1 > num3:
-    print("O maior número é {}".format(num1))
+# if num1 > num2 and num1 > num3:
+#     print("O maior número é {}".format(num1))
 
-elif num2 > num1 and num2 > num3:
-    print("O maior número é {}".format(num2))
+# elif num2 > num1 and num2 > num3:
+#     print("O maior número é {}".format(num2))
 
-elif num3 > num1 and num3 > num2:
-    print("O maior número é {}".format(num3))
+# elif num3 > num1 and num3 > num2:
+#     print("O maior número é {}".format(num3))
+
+# # 4.Crie um programa Python que solicite ao usuário uma temperatura em Fahrenheit e converta para Celsius.
+# f = float(input("Digite a temperatura em Fahrenheit que deseja convertar para Celsius: "))
+
+# C = (f-32)/1.8
+
+# print(f"A temperatura em Celsius é {C:.2f}°C")
+
+
+#5.Escreva um programa Python que determine se um número fornecido pelo usuário é positivo, negativo ou zero.
+
+
+numero = float(input("Digite um número: "))
+
+if numero > 0:
+    print("O número é positivo.")
+elif numero < 0:
+    print("O número é negativo.")
+else:
+    print("O número é zero.")
+
 
