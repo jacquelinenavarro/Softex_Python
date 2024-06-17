08d6c9001e6b714cc54bb50a2e00603ad0dfdb45
Jacqueline Navarro <154712329+jacquelinenavarro@users.noreply.github.com>
2024-06-17 17:58:09 +0000

    metroscent170624

---
+++ metroscent.py
@@ -0,0 +1,5 @@
+#Faça um programa que converta metros para centímetros:
+
+metros = float(input("Digite o valor em metros que você deseja converter para centímetros: "))
+centimetros = metros * 100
+print (f"{metros:.2f} metro(s) equivale(m) a {centimetros:.2f} centímetros.")
