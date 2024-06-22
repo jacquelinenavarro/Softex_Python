5c6c852f8e3ce6eed3718c91c6b37f073a2e3f53
Jacqueline Navarro <154712329+jacquelinenavarro@users.noreply.github.com>
2024-06-22 01:27:07 +0000

    19062024positivonegativo

---
+++ 19062024positivonegativo.py
@@ -0,0 +1,9 @@
+# Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+
+numero = float(input("Digite um número para saber se ele é positivo ou negativo: "))
+
+if numero >=0:
+    print(f"O número {numero} é positivo.")
+
+else:
+    print(f"O número {numero} é negativo.")
