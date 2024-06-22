c8273e544330aabd8843bf11c0eddd9971e173a9
Jacqueline Navarro <154712329+jacquelinenavarro@users.noreply.github.com>
2024-06-22 01:51:13 +0000

    19062024oplogicoor

---
+++ 19062024oplogicoor.py
@@ -0,0 +1,15 @@
+# O operador 'or' retorna 'True' se pelo menos uma das expressões booleanas combinadas por ele for verdadeira.
+#Exemplo1:
+# idade = 20
+# possui_carta_credito = False
+
+# if idade >= 18 or possui_carta_credito:
+#     print("Você pode fazer compras online!")
+
+#xxxxxxxxxxxxxxxxxxxxxxxxx#xxxxxxxxxxxxxxxxxxxxxxxxx#
+
+#Exemplo2:
+
+dia = "Sábado"
+if dia == "Sábado" or dia == "Domingo":
+    print("É fim de semana!")
