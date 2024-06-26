5f5bfddfabbdfad243dd0b7ff38a6e83a2cf1015
Jacqueline Navarro <154712329+jacquelinenavarro@users.noreply.github.com>
2024-06-26 18:28:44 +0000

    26062024while

---
+++ semana25a28062024/26062024while.py
@@ -0,0 +1,17 @@
+# Estruturas de repetição while (enquanto).
+
+# i = 0
+# while(i < 5):
+#     print("O valor de i = ", i)
+#     i = i +1
+# print("Programa finalizado com sucesso!")
+
+i = True
+j = 1
+while(i):
+    print(j)
+    j = j + 1
+    if (j == 5):
+        i = False
+print("Programa finalizado com sucesso!")
+
