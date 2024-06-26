84e412c806eacf115e34a6e849434fe61cd5d713
Jacqueline Navarro <154712329+jacquelinenavarro@users.noreply.github.com>
2024-06-26 18:39:05 +0000

    26062024for

--- semana25a28062024/26062024while.py
+++ semana25a28062024/26062024while.py
@@ -6,12 +6,19 @@
 #     i = i +1
 # print("Programa finalizado com sucesso!")
 
-i = True
-j = 1
-while(i):
-    print(j)
-    j = j + 1
-    if (j == 5):
-        i = False
-print("Programa finalizado com sucesso!")
+# i = True
+# j = 1
+# while(i):
+#     print(j)
+#     j = j + 1
+#     if (j == 5):
+#         i = False
+# print("Programa finalizado com sucesso!")
+
+#Estruturas de repetição for (para).
+
+lista = ['Gol', 'Palio', 'Argus', 'HB20']
+
+for carros in lista: 
+    print("Lista de carros: ", carros)
 
