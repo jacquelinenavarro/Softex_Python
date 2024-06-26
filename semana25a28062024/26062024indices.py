3734e22be5536f533d1ebc361e4f80e7e62738be
Jacqueline Navarro <154712329+jacquelinenavarro@users.noreply.github.com>
2024-06-26 18:02:43 +0000

    26062024indices

---
+++ semana25a28062024/26062024indices.py
@@ -0,0 +1,20 @@
+lista = ['Gol','Palio','Argus','HB20']
+
+#print(f'Conteúdo da lista: {lista}')
+# print(f'Primeiro elemento da lista: {[0]}')
+# print(f'Segundo elemento da lista: {[1]}')
+# print(f'Terceiro elemento da lista: {[2]}')
+
+#Cadastrar o automóvel Corsa:
+
+
+# Excluir o automóvel Gol
+
+lista.remove('Gol')
+# Listar todos os automóveis
+#print(lista)
+lista.append('Corsa')
+print(lista)
+
+
+
