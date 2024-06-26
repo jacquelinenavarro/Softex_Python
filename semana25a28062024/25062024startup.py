c55389b0f5e34414b19e6f50e8b3a44ab1cdcb82
Jacqueline Navarro <154712329+jacquelinenavarro@users.noreply.github.com>
2024-06-26 17:35:55 +0000

    26062024indices

---
+++ semana25a28062024/25062024startup.py
@@ -0,0 +1,7 @@
+lista = ['Gol','Palio','Argus','HB20']
+
+print(f'Conteúdo da lista: {lista}')
+print(f'Primeiro elemento da lista: {[0]}')
+print(f'Segundo elemento da lista: {[1]}')
+print(f'Terceiro elemento da lista: {[2]}')
+
