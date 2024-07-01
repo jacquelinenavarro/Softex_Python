13225228f6672f6ad9f0f689e8c244a9e4df4c99
Jacqueline Navarro <154712329+jacquelinenavarro@users.noreply.github.com>
2024-07-01 18:22:04 +0000

    01072024conjunto

---
+++ semana01a05072024/01072024conjunto.py
@@ -0,0 +1,16 @@
+#Conjuntos
+
+# Conjuntos, em Python, são coleções de elementos únicos e não ordenados. São úteis para realizar operações
+# como união,interseção e diferença entre coleções.
+
+# Operações comuns com conjuntos:
+conjunto = {1, 2, 3, 4, 5}
+print(conjunto)
+
+#Adicionar elementos:
+conjunto.add(6)
+print(conjunto)
+
+#Remover elementos:
+conjunto.remove(2)
+print(conjunto)
