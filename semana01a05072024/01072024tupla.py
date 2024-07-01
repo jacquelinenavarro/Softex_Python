d8f0511c203ae2a1653cf3cca4d77c023df13b06
Jacqueline Navarro <154712329+jacquelinenavarro@users.noreply.github.com>
2024-07-01 18:20:54 +0000

    01072024tupla

---
+++ semana01a05072024/01072024tupla.py
@@ -0,0 +1,15 @@
+# Tupla
+
+# Exemplo:
+# tupla = (1,2,3,4,5)
+# Operações comuns com tuplas:
+# Acessar elementos por índice: tupla[0] retorna o primeiro elemento.
+
+tupla = (1,2,3,4,5)
+print(tupla[0])
+
+# Iterar sobre os elementos: for item in tupla:
+
+for lista in tupla:
+    print("Tupla: ", lista)
+
