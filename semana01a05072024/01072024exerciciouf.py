1c7dea1f91060449e4957bff039715407a47b880
Jacqueline Navarro <154712329+jacquelinenavarro@users.noreply.github.com>
2024-07-01 18:24:22 +0000

    01072024exerciciouf

---
+++ semana01a05072024/01072024exerciciouf.py
@@ -0,0 +1,19 @@
+# Exercício:
+
+# Criar uma tupla.
+# Uf do Brasil.
+# Armazenar as UF na tupla.
+# Listar todas as UF.
+
+# Norte: Amazonas — Manaus; Amapá — Macapá; ...
+# Sul: Paraná — Curitiba; Rio Grande do Sul — Porto Alegre; ...
+# Sudeste: São Paulo — São Paulo; Rio de Janeiro — Rio de Janeiro; ...
+# Centro-Oeste: Goiás — Goiânia; Mato Grosso — Cuiabá; ...
+# Nordeste: Pernambuco — Recife; Bahia — Salvador;
+
+
+tupla = ('AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA','PB', 'PE','PI', 'PR','RJ', 'RN', 'RO', 'RS', 'SC', 'SE', 'SP', 'TO')
+
+
+for lista in tupla:
+    print("UF: ", lista)
