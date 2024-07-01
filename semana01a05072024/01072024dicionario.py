0dfd5f09211650b840eb6932323d9d30be64113c
Jacqueline Navarro <154712329+jacquelinenavarro@users.noreply.github.com>
2024-07-01 18:23:03 +0000

    01072024dicionario

---
+++ semana01a05072024/01072024dicionario.py
@@ -0,0 +1,15 @@
+#Dicionário
+
+# Dicionários são estruturas de dados que mapeiam chaves a valores. São muito eficientes para buscas
+# rápidas e associações de informações.
+
+dicionario = {'chave1':1, 'chave2':2, 'chave3':3 }
+print(dicionario)
+
+#Exemplo:
+#Operações comuns com dicionários: Acessar valor por chave:
+print(dicionario ['chave1'])
+
+#Acessar valor por chave: 
+dicionario['chave4'] = 4
+print(dicionario)
