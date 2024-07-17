c08b46de6c1098ccf403e5d1732da8152aa663f6
Jacqueline Navarro <154712329+jacquelinenavarro@users.noreply.github.com>
2024-07-17 22:21:49 +0000

    tabuada17072024

--- semana17a19072024/tabuada.py
+++ semana17a19072024/tabuada.py
@@ -9,16 +9,10 @@
 #  5 X 10 = 50
 
 
-# import funcoes
-
-# n1 = float(input("Digite o número o qual deseja gerar a tabuada (entre 1 e 10): "))
-
-# print(f'{n1} X {i} = ', funcoes.tabuada(n1))
-
 import funcoes
 
 n1 = int(input("Digite o número para gerar a tabuada (entre 1 e 10): "))
 
 print(f'O resultado da tabuada de {n1} é:')
-print(funcoes.tabuada(n1))
+funcoes.tabuada(n1)
  
