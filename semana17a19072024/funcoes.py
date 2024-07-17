698b315903ab3bda529943f9337ae74054bd685f
Jacqueline Navarro <154712329+jacquelinenavarro@users.noreply.github.com>
2024-07-17 18:08:32 +0000

    modulo17072024

---
+++ semana17a19072024/funcoes.py
@@ -0,0 +1,19 @@
+
+def soma(parametro1, parametro2):
+    soma = parametro1 + parametro2
+    return soma
+
+def subtracao(parametro1, parametro2):
+    subtracao = parametro1 - parametro2
+    return subtracao
+
+def multiplicacao(parametro1, parametro2):
+    multiplicacao = parametro1 * parametro2
+    return multiplicacao
+
+def divisao(parametro1, parametro2):
+    divisao = parametro1 / parametro2
+    return divisao
+
+
+
---
+++ semana17a19072024/inicio.py
@@ -0,0 +1,35 @@
+import funcoes
+
+# n1 = 10
+# n2 = 20
+# print(funcoes.soma(n1,n2))
+# print(funcoes.multiplicacao(n1,n2))
+
+import funcoes
+
+n1 = float(input("Digite o primeiro número: "))
+n2 = float(input("Digite o segundo número: "))
+
+while True:
+    print("\n========= Menu ========")
+    print("1. Para somar")
+    print("2. Para subtrair")
+    print("3. Para multiplicar")
+    print("4. Para dividir")
+    print("5. Você saiu do sistema com sucesso!")
+
+    opcao = input("Escolha uma opção: ")
+
+    if opcao == '1':
+        print(f'A soma entre os números {n1} e {n2} é ', funcoes.soma(n1, n2))
+    elif opcao == '2':
+        print(f'A subtração entre os números {n1} e {n2} é ', funcoes.subtracao(n1, n2))
+    elif opcao == '3':
+        print(f'A multiplicação entre os números {n1} e {n2} é ', funcoes.multiplicacao(n1, n2))
+    elif opcao == '4':
+        print(f'A divisão entre os números {n1} e {n2} é ', funcoes.divisao(n1, n2))
+    elif opcao == '5':
+        print("Você saiu do sistema!")
+        break
+    else:
+        print("Opção inválida. Por favor, escolha uma opção válida.")
