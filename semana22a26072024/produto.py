81459e0958fdaddf63f95605c9ba8267e0bbc424
Jacqueline Navarro <154712329+jacquelinenavarro@users.noreply.github.com>
2024-07-24 18:01:52 +0000

    lojaprodutos2407

---
+++ semana22a26072024/produto.py
@@ -0,0 +1,8 @@
+# Projetos de Lógica de Programação com Python
+
+# Projeto escolhido: 10 - Loja e Produtos
+
+# Descrição: Uma loja vende vários produtos.
+# Cada produto é vendido por uma única loja.
+# Relação: Uma loja -> Muitos produtos.
+
