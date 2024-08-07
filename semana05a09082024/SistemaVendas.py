e604c27c9e0177ed98909a900a78719a317a55f7
Jacqueline Navarro <154712329+jacquelinenavarro@users.noreply.github.com>
2024-08-07 19:11:55 +0000

    add sist vendas 0708

--- semana05a09082024/SistemaVendas.py
+++ semana05a09082024/SistemaVendas.py
@@ -1,37 +1,36 @@
+#Privado
+
+#Classe primária ou superclasse
 class Usuario():
-    # Construtor
-    def __init__(self):
-        self.__numeroUsuario = ""
-        
-    # Método para alterar o comportamento da classe ou objeto
-    def get_numeroUsuario(self):
+    #Construtor
+    def __init__(self, numerousuario):
+        self.__numeroUsuario = numerousuario
+
+    #Método alterar o comportamento da classe ou objeto
+    def get_numeroUsario(self):
         return self.__numeroUsuario
-    
-    # Método para recuperar o comportamento da classe
-    def set_numeroUsuario(self, v_numeroUsuario):
-        self.__numeroUsuario = v_numeroUsuario
-    
-# Instância
-usuario1 = Usuario()
-usuario1.set_numeroUsuario('001')
-print(usuario1.get_numeroUsuario())
-
-usuario2 = Usuario()
-usuario2.set_numeroUsuario('002')
-print(usuario2.get_numeroUsuario())
-
-usuario3 = Usuario()
-usuario3.set_numeroUsuario('003')
-print(usuario3.get_numeroUsuario())
-
-
-#Classe secundaria ou subclasse:
+   
+    #Método para recuperar o comportamento da classe
+    def set_numeroUsuaio(self, numeroUsuario):
+        self.__numeroUsuario = numeroUsuario
+   
+#Classe secundária ou subclasse
 class Administrador(Usuario):
-    def __init__(self):
-        return "Administrador"
+
+    def __init__(self, numeroUsuario, n):
+        super().__init__(numeroUsuario)
+        self.__nomeAdm = n
+   
+    def get_nomeAdministrador(self):
+        return self.__nomeAdm
+    #Atribuir
+    def set_nomeAdministrador(self, n):
+        self.__nomeAdm  = n
 
 
-administrador = Usuario("Andre")
-administrador.set_numeroUsuario('002')
-administrador.set_numeroUsuario('Softex')
-print(administrador.get_numeroUsuario)
+administrador = Administrador("002","Paulo")
+#administrador.set_numeroUsuaio('002')
+#administrador.set_nomeAdministrador('Softex')
+
+print(administrador.get_numeroUsario())
+print(administrador.get_nomeAdministrador())
